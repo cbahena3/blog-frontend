53aafc48a3ebcc9ff2b20a1fe6f08551617629d2
Cristian B <cris.bahena77@gmail.com>
2023-12-15 15:00:58 -0600

    added css

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -5,9 +5,9 @@ export function PostsIndex(props) {
     <div id="posts-index">
       <h1>All Posts</h1>
       {props.posts.map(post => <div key={post.id}>
-        <h2>title: {post.title}</h2>
+        <h2>Vehicle: {post.title}</h2>
         <img src={post.image_url} />
-        <p>body: {post.body}</p>
+        <p>Description: {post.body}</p>
       </div>
       )}
     </div>
