8458f02efc837a099f8fee672275ccd4fe0792dd
Cristian B <cris.bahena77@gmail.com>
2023-12-18 12:55:06 -0600

    added useState

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,18 +1,21 @@
 import { PostsIndex } from './PostsIndex';
 import { PostsNew } from './PostsNew';
 import axios from "axios";
+import { useState } from 'react';
 
 export function Content() {
-  let posts = [];
+  const [posts, setPosts] = useState([]);
 
   const handleIndexPosts = () =>{
-    axios.get("http://localhost:3000/posts.json").then(response => console.log(response.data))
+    axios.get("http://localhost:3000/posts.json").then(response => {console.log(response.data)
+    setPosts(response.data)
+    })
   };
 
   return (
     <div>
       <PostsNew />
-      <button onClick={handleIndexPosts}> PRESS ME FOR DATA </button>
+      <button onClick={handleIndexPosts}> PRESS FOR ALL POSTS </button>
       <PostsIndex posts={posts} />
     </div>
   );
